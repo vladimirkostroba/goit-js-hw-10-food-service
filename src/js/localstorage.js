1775140faa579becfabc8e021d9cc637fd956f01
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2020-07-26 19:11:28 +0300

    fixed

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -9,33 +9,26 @@ switchRef.addEventListener('change', handleSwitchChange);
 
 function handleSwitchChange(event) {
   if (event.target.checked) {
-    darkTheme();
+    changeTheme(themeColor.dark, themeColor.light);
   } else {
-    lightTheme();
+    changeTheme(themeColor.light, themeColor.dark);
   }
 
   localStorage.setItem(
     'theme',
-    // JSON.stringify(themeColor),
     event.target.checked ? themeColor.dark : themeColor.light,
   );
 }
 
-function darkTheme() {
-  document.body.classList.add(themeColor.dark);
-  document.body.classList.remove(themeColor.light);
-}
-
-function lightTheme() {
-  document.body.classList.add(themeColor.light);
-  document.body.classList.remove(themeColor.dark);
+function changeTheme(newTheme, oldTheme) {
+  document.body.classList.add(newTheme);
+  document.body.classList.remove(oldTheme);
 }
 
 const savedTheme = localStorage.getItem('theme');
 
 if (savedTheme) {
-  // const parsedTheme = JSON.parse(savedTheme);
   document.body.classList.add(savedTheme);
 
-  switchRef.checked = savedTheme;
+  switchRef.checked = savedTheme === themeColor.dark;
 }
